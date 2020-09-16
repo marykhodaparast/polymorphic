c56c78f84d71797b99252421b37c4388673ebb1f
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-16 15:13:57 +0430

    Deleting data 16-112

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ Route::get('/', function () {
 });
 Route::get('/create', function () {
     $staff = Staff::findOrFail(1);
-    $staff->photos()->create(['path' => 'Edwin.jpg']);
+    $staff->photos()->create(['path' => 'Edwin2.jpg']);
 });
 Route::get('/read', function () {
     $staff = Staff::findOrFail(1);
@@ -31,4 +31,11 @@ Route::get('/update', function () {
     $photo = $staff->photos()->whereId(1)->first();
     $photo->path = "Updated example.jpg";
     $photo->save();
+});
+Route::get('/delete',function(){
+    $staff = Staff::findOrFail(1);
+    //$staff->photos()->delete();
+    $staff->photos()->whereId(3)->delete();
+    //for example you can say $staff->photos->whereName('bad_photo.jpg')->delete();
+
 });
