103ea31e14124e350d6c1622e022acfa55385262
maryam khodaparast <khcomputergirl@gmail.com>
2020-09-16 12:21:33 +0430

    relationships and mass assignment 16-108

--- app/Photo.php
+++ app/Photo.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Photo extends Model
 {
-    //
+    protected $fillable = ['path'];//mass assignment
+    public function imageable(){
+       return $this->morphTo();
+    }
 }
--- app/Product.php
+++ app/Product.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Product extends Model
 {
-    //
+    protected $fillable = ['name'];
+    
+    public function photos(){
+        return $this->morphMany('App\Photo','imageable');
+     }
 }
